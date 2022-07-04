493c1e15e7e296d500c233c2347e4e43013e89ab
zhiwei <71208463+zhiwei-zhiwei@users.noreply.github.com>
2022-07-04 16:38:17 -0500

    Update combinationSum.py

--- 39. Combination Sum/combinationSum.py
+++ 39. Combination Sum/combinationSum.py
@@ -2,17 +2,22 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
 
-        def dfs(i, curr, total):
+        def dfs(index, temp, total):
             if total == target:
-                res.append(curr.copy())
+                res.append(temp[:])
                 return
-            if i >= len(candidates) or total > target:
+            if total > target or index >= len(candidates):
                 return
             
-            curr.append(candidates[i])
-            dfs(i,curr,total+candidates[i])
-            curr.pop()
-            dfs(i+1, curr, total)
-
+            temp.append(candidates[index])
+            dfs(index, temp, total + candidates[index])
+            temp.pop()
+            dfs(index+1, temp, total)
+            # index+1 here -> in order to make sure that the next backtrace will not go throught the same element that already used before
+            # eg. candidates = [2,3,6,7], target = 7
+            #          [2,2]
+            #         /     \ (index+i) make sure it will not read the 2 again
+            #    [2,2,2]    [2,2,3]
+        
         dfs(0,[],0)
         return res
