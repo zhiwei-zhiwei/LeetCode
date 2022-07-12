4e1872ecfac02294f14a593a2f39ff65d4e4cbd0
zhiwei <71208463+zhiwei-zhiwei@users.noreply.github.com>
2022-07-11 20:36:04 -0500

    Add files via upload

---
+++ 160. Intersection of Two Linked Lists/Solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        # hashMap = set()
+        # while headA:
+        #     hashMap.add(headA)
+        #     headA = headA.next
+        # while headB:
+        #     if headB in hashMap:
+        #         return headB
+        #     headB = headB.next
+        # return None
+
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
