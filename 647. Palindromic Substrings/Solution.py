5abf040bdc6a38987e6ac6e6ecc80a971b91238f
zhiwei <71208463+zhiwei-zhiwei@users.noreply.github.com>
2022-08-07 14:42:35 -0500

    Create Solution.py

---
+++ 647. Palindromic Substrings/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(2 * len(s) - 1):
+            left = i // 2
+            right = left + i % 2
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+                res += 1
+
+        return res
