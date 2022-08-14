a765c466a1cede51e0536ac340aeeb71aeb73b15
zhiwei <71208463+zhiwei-zhiwei@users.noreply.github.com>
2022-08-14 17:28:42 -0500

    Update permute.py

--- 46. Permutations/permute.py
+++ 46. Permutations/permute.py
@@ -6,12 +6,6 @@ class Solution:
                 res.append(tmp)
                 return 
             for i in range(len(nums)):
-                # q = nums[:i]
-                # w = nums[i+1:]
-                qw = nums[:i] + nums[i+1:]
-                # e = tmp
-                # r = [nums[i]]
-                er = tmp + [nums[i]]
                 backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
         backtrack(nums, [])
         return res
