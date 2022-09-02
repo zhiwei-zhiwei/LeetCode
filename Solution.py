25e1261e6886f9d16d18bf1364179f2f5712e8ca
zhiwei <71208463+zhiwei-zhiwei@users.noreply.github.com>
2022-09-02 14:58:08 -0500

    Delete Solution.py

--- Solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        temp = 0
-        for i,v in enumerate(nums):
-            if temp >= i and i + v > temp:
-                temp = i + v
-        return temp >= len(nums)-1
